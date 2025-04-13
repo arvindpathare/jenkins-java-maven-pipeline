36ef49c0a8d4c42824368654fece0425d0d87abd
arvindpathare <39178975+arvindpathare@users.noreply.github.com>
2025-04-13 19:43:18 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,7 @@
-package com.example;
+package com.mycompany.app;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public String getMessage() {
+        return "Hello World!";
     }
 }
